703e104fe2ad352ee206bca011d10a63792c9a07
mayssane <mokhtarimay@gmail.com>
2024-02-06 21:03:32 +0000

    Generic views

--- polltastic/polls/views.py
+++ polltastic/polls/views.py
@@ -4,7 +4,25 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.urls import reverse
 from .models import Choice, Question
+from django.views import generic
 
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset (self):
+        """return the last five questions"""
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+"""
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     #template = loader.get_template("polls/index.html")
@@ -27,6 +45,7 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/results.html", {"question": question})
 
+"""
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
