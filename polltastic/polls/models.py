99c7bdec93bf81d1a0f141b13b401d93580ae4d1
mayssane <mokhtarimay@gmail.com>
2024-02-07 22:29:45 +0000

    Unit Testing : model questions

--- polltastic/polls/models.py
+++ polltastic/polls/models.py
@@ -8,7 +8,18 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        """
+        recent = True
+        if self.pub_date > timezone.now():
+            recent = False
+        elif self.pub_date < timezone.now() - datetime.timedelta(days=1):
+            recent = False
+        
+        return recent
+        """
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        # return self.pub_date +  datetime.timedelta(days=1)  <= timezone.now()
 
     def __str__(self):
         return self.question_text
