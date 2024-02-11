f7142d08905eb5d635b3c1015845610e974e02fa
mayssane <mokhtarimay@gmail.com>
2024-02-11 20:15:46 +0000

    Customised admin and template style

--- polltastic/polltastic/settings.py
+++ polltastic/polltastic/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'polltastic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
